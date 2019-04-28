ae70072c3f30ce89fd91510b8606a0ca7adef49f
babita27 <40436442+babita27@users.noreply.github.com>
2019-04-28 13:20:14 +0530

    Update HelloController.java

--- src/main/java/com/g33kzone/helloworld/controller/HelloController.java
+++ src/main/java/com/g33kzone/helloworld/controller/HelloController.java
@@ -22,7 +22,7 @@ public class HelloController {
 
 		Employee emp = new Employee();
 		emp.setName("emp1");
-		emp.setDesignation("manager");
+		emp.setDesignation("manager")
 		emp.setEmpId("1");
 		emp.setSalary(3000);
 
